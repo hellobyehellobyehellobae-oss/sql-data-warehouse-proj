e70c0f48bb978003d6d3c67e49c412241ab227ad
hellobyehellobyehellobae-oss <hellobyehellobyehellobae@gmail.com>
2026-01-08 22:26:47 +0200

    Update initial_database.sql

--- scripts/initial_database.sql
+++ scripts/initial_database.sql
@@ -4,12 +4,4 @@ CREATE OR REPLACE SCHEMA WAREHOUSE_DB.BRONZE;
 CREATE OR REPLACE SCHEMA WAREHOUSE_DB.SILVER;
 CREATE OR REPLACE SCHEMA WAREHOUSE_DB.GOLD;
 
-CREATE TABLE bronze.crm_cust_info (
-    cst_id INT,
-    cst_key NVARCHAR(50),
-    cst_firstname NVARCHAR(50),
-    cst_lastname NVARCHAR(50),
-    cst_material_status NVARCHAR(50),
-    cst_gndr NVARCHAR(50),
-    cst_create_date DATE
-);
+
