f06ecec5ffe5d81eefd89a31a893942b78073084
hellobyehellobyehellobae-oss <hellobyehellobyehellobae@gmail.com>
2026-01-14 23:35:14 +0200

    Update table_creation.sql

--- scripts/bronze/table_creation.sql
+++ scripts/bronze/table_creation.sql
@@ -1,4 +1,13 @@
 
+CREATE OR REPLACE TABLE bronze.crm_prd_info (
+    prd_id INT,
+    prd_key NVARCHAR(50),
+    prd_nm NVARCHAR(50),
+    prd_cost INT,
+    prd_line NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt DATETIME
+);
 CREATE TABLE bronze.crm_cust_info (
     cst_id INT,
     cst_key NVARCHAR(50),
