c80cfb1f3687ee6d2a83244535846148081ba02a
hellobyehellobyehellobae-oss <hellobyehellobyehellobae@gmail.com>
2026-01-29 12:33:23 +0200

    Create change_over_time.sql

---
+++ Analytics/change_over_time.sql
@@ -0,0 +1,31 @@
+SELECT
+    YEAR(order_date) AS order_year,
+    MONTH(order_date) AS order_month,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date), MONTH(order_date)
+ORDER BY YEAR(order_date), MONTH(order_date);
+
+SELECT
+    DATE_TRUNC('MONTH', order_date) AS order_month,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATE_TRUNC('MONTH', order_date)
+ORDER BY DATE_TRUNC('MONTH', order_date);
+
+
+SELECT
+    TO_VARCHAR(order_date, 'YYYY-MON') AS order_month,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY TO_VARCHAR(order_date, 'YYYY-MON')
+ORDER BY TO_VARCHAR(order_date, 'YYYY-MON');
